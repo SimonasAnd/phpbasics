ea240cb95243432e55653b901df7cf2101948544
SimonasAnd <scorpionqp@gmail.com>
2019-10-29 17:27:38 +0200

    comment v2

--- index.php
+++ index.php
@@ -3,17 +3,9 @@
         <title>Komentarai</title>
     </head>
     <body>
-        <h1>
-            Dainų Tekstai
-        </h1>
-        <h2>
-            Paragrafo Daina
-        </h2>
-        <p>
-            Aš toks vienišas jaunas paragrafas...
-        </p>
-        <p>
-            Mindau svetimo kodo gatves...
-        </p>
+        <h1>Dainų Tekstai</h1>
+        <h2>Paragrafo Daina</h2>
+        <p>Aš toks vienišas jaunas paragrafas...</p>
+        <p>Mindau svetimo kodo gatves...</p>
     </body>
 </html>
