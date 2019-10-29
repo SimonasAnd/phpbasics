7473668ff156867ba2b93779eb3fd91903145ce3
SimonasAnd <scorpionqp@gmail.com>
2019-10-29 15:42:53 +0200

    Praktika 2

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <html>
     <head>
-        <title>Aš valdau HTML!</title>
+        <title>Aš valdau HTML! <?php print"ir PHP"?></title>
     </head>
     <body>
-        <h1><b>Simonas</b> - HTML asas</h1>
-        <p>Moku kurti statinį turinį.</p>
+        <h1><b>Simonas</b> - HTML <?php print"ir PHP"?> asas</h1>
+        <p>
+            Moku kurti statinį turinį.<br>
+            Atidaryta <?php print date('Y-m-d H:i:s') ?>
+        </p>
+        
     </body>
 </html>
