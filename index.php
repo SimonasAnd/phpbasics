1f3f51e04cf0d0e8776334f35cdab8402f5889d5
SimonasAnd <scorpionqp@gmail.com>
2019-10-29 15:05:14 +0200

    Praktika 1

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php print"GIT?"; ?>
+<html>
+    <head>
+        <title>Aš valdau HTML!</title>
+    </head>
+    <body>
+        <h1><b>Simonas</b> - HTML asas</h1>
+        <p>Moku kurti statinį turinį.</p>
+    </body>
+</html>
