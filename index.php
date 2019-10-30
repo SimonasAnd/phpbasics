0d38c71128ba8ca6bbba09cd2d899949d3a1ab3a
SimonasAnd <scorpionqp@gmail.com>
2019-10-30 13:52:25 +0200

    nauja diena

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <h1><b>Simonas</b> - Galbūt turėsiu <?php print rand(1, 5); ?> vaikų(us)!</h1>
         <p>
             D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . ' year')); ?>
+            new day
         </p>
     </body>
 </html>
