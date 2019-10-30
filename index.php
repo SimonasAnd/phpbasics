5bd582be380330cc5489bd388fa765c734bc4ccf
SimonasAnd <scorpionqp@gmail.com>
2019-10-30 15:23:15 +0200

    rand dice

--- index.php
+++ index.php
@@ -2,46 +2,34 @@
     <head>
         <title>PHP</title>
         <style>
-            .dice1{
+            .dice{
                 display:block;
                 width:112px;
                 height:112px;
+            }
+            
+            .dice-1{
                 background-image: url("https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png");
             }
-            .dice2{
-                display:block;
-                width:112px;
-                height:112px;
+            .dice-2{
                 background-image: url("https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png");
             }
-            .dice3{
-                display:block;
-                width:112px;
-                height:112px;
+            .dice-3{
                 background-image: url("https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png");
             }
-           .dice4{
-                display:block;
-                width:112px;
-                height:112px;
+           .dice-4{
                 background-image: url("https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png");
             }
-            .dice5{
-                display:block;
-                width:112px;
-                height:112px;
+            .dice-5{
                 background-image: url("https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png");
             }
-            .dice6{
-                display:block;
-                width:112px;
-                height:112px;
+            .dice-6{
                 background-image: url("https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png");
             }
         </style>
     </head>
     <body>
-        <div class="dice<?php print rand(1,6) ?>">
+        <div class="dice dice-<?php print rand(1,6) ?>">
         </div>
     </body>
 </html>
