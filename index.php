bd8a26eb8f616f527070aa106646b32c46637296
SimonasAnd <scorpionqp@gmail.com>
2019-10-30 16:53:12 +0200

    bomb boom

--- index.php
+++ index.php
@@ -6,18 +6,24 @@
                 background-image: url("https://files.gamebanana.com/img/ico/sprays/4ea33068c0dcc.png");
                 background-size: cover;
                 background-repeat: no-repeat;
-            }
-            
-            .size{
-                display:block;
+                display: block;
                 width: calc(2.56px * <?php print date('s') ?>);
                 height: calc(1.9px * <?php print date('s') ?>);
             }
+            
+            .boom00{
+                background-image: url("https://upload.wikimedia.org/wikipedia/commons/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg");
+                background-size: cover;
+                display: block;
+                width:500px;
+                height:500px;
+            }
+            
         </style>
     </head>
     <body>
-        <div class="size bomb">
+        <div class="size bomb boom<?php print date('s'); ?>">
         </div>
-        <?php print date('s') . 's' ?>
+        <p><?php print date('s')?></p>
     </body>
 </html>
