9b94119c0a24327d1dd07b95202e653beb3e7008
SimonasAnd <scorpionqp@gmail.com>
2019-10-30 16:23:34 +0200

    bomb size

--- index.php
+++ index.php
@@ -2,34 +2,23 @@
     <head>
         <title>PHP</title>
         <style>
-            .dice{
-                display:block;
-                width:112px;
-                height:112px;
+            .bomb{
+                background-image: url("https://files.gamebanana.com/img/ico/sprays/4ea33068c0dcc.png");
+                background-size: cover;
+                background-repeat: no-reapeat;
+            /*256 190*/
             }
             
-            .dice-1{
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png");
-            }
-            .dice-2{
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png");
-            }
-            .dice-3{
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png");
-            }
-           .dice-4{
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png");
-            }
-            .dice-5{
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png");
-            }
-            .dice-6{
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png");
+            .size{
+                display:block;
+                width: calc(2.56px * <?php print date('s') ?>);
+                height: calc(1.9px * <?php print date('s') ?>);
             }
         </style>
     </head>
     <body>
-        <div class="dice dice-<?php print rand(1,6) ?>">
+        <div class="size bomb">
         </div>
+        <?php print date('s') ?>
     </body>
 </html>
