857f066db3ca9d8fafe7a262a7b91fc7b4a94515
SimonasAnd <scorpionqp@gmail.com>
2019-10-30 14:45:00 +0200

    rand spalvos v2

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     <head>
         <title>PHP</title>
     </head>
-    <body style="background-color: rgba(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>">
+    <body style="background-color: rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>">
         <h1 style="font-size: <?php print rand(15, 50); ?>">Aš keičiu savo dydį</h1>
-        <p style="color: rgba(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>">
+        <p style="color: rgb(<?php print rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255); ?>">
             Aš keičiu savo spalvą!
         </p>
     </body>
