2289d9c7101cc7023e86e0136bb66021f5385d3e
SimonasAnd <scorpionqp@gmail.com>
2019-10-31 16:13:28 +0200

    kodo tvarkymas v5

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <?php
-    $num_1 = rand(1, 100);
-    $num_2 = rand(101, 200);
-    $num_3 = rand(201, 300);
+    $paimta = rand(1, 100);
+    $dviem_grazinsi = rand(101, 200);
+    $vienu_grazinsi = rand(201, 300);
     
-    $h3_1 = "Jei paėmei $num_1 jievru";
-    $h3_2 = "Su dviem kabančiais grąžinsi $num_3";
-    $h3_3 = "Su vienu kabančiu grąžinsi $num_2";
+    $h3_1 = "Jei paėmei $paimta jievru";
+    $h3_2 = "Su dviem kabančiais grąžinsi $dviem_grazinsi";
+    $h3_3 = "Su vienu kabančiu grąžinsi $vienu_grazinsi";
 ?>
 <html>
     <head>
         <title>PHP</title>
     </head>
-    <body>
-        <div class="body_1">
-            <h1>Skolos skaiciuokle</h1>
-            <h3><?php print $h3_1; ?></h3>
-            <h3><?php print $h3_2; ?></h3>
-            <h3><?php print $h3_3; ?></h3>
-        </div>
+        <h1>Skolos skaiciuokle</h1>
+        <h3><?php print $h3_1; ?></h3>
+        <h3><?php print $h3_2; ?></h3>
+        <h3><?php print $h3_3; ?></h3>
     </body>
 </html>
