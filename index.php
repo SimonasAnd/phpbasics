b1e21db66c880c667673ac0cf6f31c2772dc4a9a
SimonasAnd <scorpionqp@gmail.com>
2019-10-31 17:45:35 +0200

    distancija if

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
-
-$distance = rand(10, 500);
+$distance = rand(10, 5000);
 $consumption = 7.5;
 $price_l = 1.3;
+$my_money = 100;
 
 $fuel_total = round($distance * $consumption / 100, 2);
 $price_trip = round($fuel_total * $price_l, 2);
+
+if($price_trip <= $my_money){
+    $iseis = 'įperkama';
+} else{
+    $iseis = 'neįperkama';
+}
 
 $h1 = 'Kelionės skaičiuoklė';
 $li_1 = "Nuvažiuota distancija: $distance";
 $li_2 = "Sunaudota $fuel_total l. kuro";
 $li_3 = "Kaina: $price_trip pinigų";
-
+$p = "Išvada: kelionė $iseis";
 ?>
 <html>
     <head>
@@ -24,5 +30,7 @@ $li_3 = "Kaina: $price_trip pinigų";
             <li><?php print $li_2; ?></li>
             <li><?php print $li_3; ?></li>
         </ul>
+        <hr>
+        <p><?php print $p; ?></p>
     </body>
 </html>
