ed4872506023d7bbea69fbde605d2e3d3c426034
SimonasAnd <scorpionqp@gmail.com>
2019-10-31 17:03:47 +0200

    distancija

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-    $bin_vol = 40;
-    $bin_heap_vol = rand(1, 15);
-    $trash_per_day = 15;
-    
-    $days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
-    
-    $h1 = 'Šiukšlių prognozė';
-    $p_1 = "Turima šiukšlinė - $bin_vol litrų";
-    $p_2 = "Žmona nieko nesako, kol neviršija $bin_heap_vol litrų";
-    $h3 = "Išvada: nieko nedarysiu $days dienas";
+
+$distance = rand(10, 500);
+$consumption = 7.5;
+$price_l = 1.3;
+
+$fuel_total = round($distance * $consumption / 100, 2);
+$price_trip = round($fuel_total * $price_l, 2);
+
+$h1 = 'Kelionės skaičiuoklė';
+$li_1 = "Nuvažiuota distancija: $distance";
+$li_2 = "Sunaudota $fuel_total l. kuro";
+$li_3 = "Kaina: $price_trip pinigų";
+
 ?>
 <html>
     <head>
@@ -16,8 +19,10 @@
     </head>
     <body>
         <h1><?php print $h1; ?></h1>
-        <p><?php print $p_1; ?></p>
-        <p><?php print $p_2; ?></p>
-        <h3><?php print $h3; ?></h3>
+        <ul>
+            <li><?php print $li_1; ?></li>
+            <li><?php print $li_2; ?></li>
+            <li><?php print $li_3; ?></li>
+        </ul>
     </body>
 </html>
