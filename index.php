1b68d904ed625b0e5de0c8a3b3ef1f891c3ce9ce
SimonasAnd <scorpionqp@gmail.com>
2019-10-31 16:37:49 +0200

    bin

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
-    $paimta = rand(1, 100);
-    $dviem_grazinsi = rand(101, 200);
-    $vienu_grazinsi = rand(201, 300);
+    $bin_vol = 40;
+    $bin_heap_vol = rand(1, 15);
+    $trash_per_day = 15;
     
-    $h3_1 = "Jei paėmei $paimta jievru";
-    $h3_2 = "Su dviem kabančiais grąžinsi $dviem_grazinsi";
-    $h3_3 = "Su vienu kabančiu grąžinsi $vienu_grazinsi";
+    $days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
+    
+    $h1 = 'Šiukšlių prognozė';
+    $p_1 = "Turima šiukšlinė - $bin_vol litrų";
+    $p_2 = "Žmona nieko nesako, kol neviršija $bin_heap_vol litrų";
+    $h3 = "Išvada: nieko nedarysiu $days dienas";
 ?>
 <html>
     <head>
         <title>PHP</title>
     </head>
-        <h1>Skolos skaiciuokle</h1>
-        <h3><?php print $h3_1; ?></h3>
-        <h3><?php print $h3_2; ?></h3>
-        <h3><?php print $h3_3; ?></h3>
+        <h1><?php print $h1; ?></h1>
+        <p><?php print $p_1; ?></p>
+        <p><?php print $p_2; ?></p>
+        <h3><?php print $h3; ?></h>
     </body>
 </html>
