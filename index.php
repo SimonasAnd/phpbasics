b9c7cd5badab2d402c9eb06e38df9540fc285766
SimonasAnd <scorpionqp@gmail.com>
2019-10-31 14:55:32 +0200

    variables

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
+<?php
+    $string = 'tekstas';
+    $int = 111;
+    $bool = True;
+    $null = null;
+?>
 <html>
     <head>
         <title>PHP</title>
-        <style>
-            .bomb{
-                background-image: url("https://files.gamebanana.com/img/ico/sprays/4ea33068c0dcc.png");
-                background-size: cover;
-                background-repeat: no-repeat;
-                display: block;
-                width: calc(2.56px * <?php print date('s') ?>);
-                height: calc(1.9px * <?php print date('s') ?>);
-            }
-            
-            .boom-00{
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg");
-                background-size: cover;
-                display: block;
-                width:500px;
-                height:500px;
-            }
-        </style>
     </head>
     <body>
-        <div class="size bomb boom-<?php print date('s'); ?>"></div>
-        <p><?php print date('s')?></p>
+        <ul>
+            <li>String tipas: <?php print $string ?></li>
+            <li>Integer tipas: <?php print $int ?></li>
+            <li>Boolean tipas: <?php print $bool ?></li>
+            <li>Null tipas: <?php print $null ?></li>
+        </ul>
     </body>
 </html>
