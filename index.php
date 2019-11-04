c01e3bd5a36cbff84d09c44d3ef676625d3cda52
SimonasAnd <scorpionqp@gmail.com>
2019-11-04 10:57:42 +0200

    ififif

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 $grizai_velai = rand(0, 1);
 $grizai_isgeres = rand(0, 1);
+$kurmiegosi = 'nemiegosi';
 
 if($grizai_velai){
     if($grizai_isgeres){
         $kaip = 'Grįžai vėlai ir išgėręs';
+        $kurmiegosi = 'miegosi';
     } else {
         $kaip = 'Grįžai vėlai';
     }
@@ -21,6 +23,7 @@ if($grizai_velai){
 
 $h1 = 'Buitinė skaičiuoklė';
 $h2 = "Situacija: $kaip";
+$h3 = "Išvada: $kurmiegosi ant sofos";
 
 ?>
 <html>
@@ -30,5 +33,6 @@ $h2 = "Situacija: $kaip";
     <body>
         <h1><?php print $h1; ?></h1>
         <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
     </body>
 </html>
