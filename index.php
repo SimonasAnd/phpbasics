f0acdd003f7854696df9972a36dc1b6179625d8d
SimonasAnd <scorpionqp@gmail.com>
2019-11-04 13:01:05 +0200

    lygus/identiski

--- index.php
+++ index.php
@@ -1,35 +1,41 @@
 <?php
-$sunny = rand(0, 1);
+$bool = true;
+$int = 1;
+$str_1 = '1';
+$str_2 = '1.23';
+$flt = 1.23;
 
-if ($sunny) {
-    $img_src = 'https://icon-library.net/images/weather-icon-sunny/weather-icon-sunny-15.jpg';
-    $h2 = 'Saulėta';
-} else {
-    $img_src = 'https://icon-library.net/images/cloudy-icon/cloudy-icon-27.jpg';
-    $h2 = 'Debesuota';
+if($bool == $int){
+    $koks_1 = 'Lygūs';
+} elseif ($bool === $int) {
+    $koks_1 = 'Identiški';
 }
+
+if($str_1 == $bool){
+    $koks_2 = 'Lygūs';
+} elseif ($str_1 === $bool) {
+    $koks_2 = 'Identiški';
+}
+
+if($flt == $str_2){
+    $koks_3 = 'Lygūs';
+} elseif ($flt == $str_2) {
+    $koks_3 = 'Identiški';
+}
+
+$li_1 = "Bool (true) ir Integer (1): $koks_1";
+$li_2 = "String (1) ir Boolean (1): $koks_2";
+$li_3 = "Float (1.23) ir String (1.23): $koks_3";
 ?>
 <html>
     <head>
         <title>PHP</title>
-        <style>
-            .flex{
-                display: flex;
-                align-items: center;
-            }
-
-            .bg_img{
-                width: 150px;
-                height: 150px;
-                background-image: url("<?php print $img_src; ?>");
-                background-size: cover;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex">
-            <div class="bg_img"></div>
-            <h2><?php print $h2; ?></h2>
-        </div>
+        <ul>
+            <li><?php print $li_1 ?></li>
+            <li><?php print $li_2 ?></li>
+            <li><?php print $li_3 ?></li>
+        </ul>
     </body>
 </html>
