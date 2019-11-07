eb6cfd92eeb1b758798de732bc6ba99c057265e8
SimonasAnd <scorpionqp@gmail.com>
2019-11-07 13:03:55 +0200

    lenktynes v3

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
 <?php
 
-$lenktynes = [
-    0 => [
-        'stiprumas_laipsn' => 90
-    ],
-    
-    1 => [
-        'stiprumas_laipsn' => -40
-    ],
-    
-    2 => [
-        'stiprumas_laipsn' => 180
-    ],
-    
-    3 => [
-        'stiprumas_laipsn' => -50
-    ],
-    
-    4 => [
-        'stiprumas_laipsn' => 180
-    ],
-    
-    5 => [
-        'stiprumas_laipsn' => 45
-    ],
-];
+$lenktynes = [ 90, -40, 180, -50, 180, 45];
 
 var_dump($lenktynes);
