e836aa683fe63c9de18cc663f4376041dd0e7301
SimonasAnd <scorpionqp@gmail.com>
2019-11-07 13:01:26 +0200

    lenktynes v2

--- index.php
+++ index.php
@@ -2,32 +2,26 @@
 
 $lenktynes = [
     0 => [
-        'puse' => 'desine',
         'stiprumas_laipsn' => 90
     ],
     
     1 => [
-        'puse' => 'kaire',
-        'stiprumas_laipsn' => 40
+        'stiprumas_laipsn' => -40
     ],
     
     2 => [
-        'puse' => 'desine',
         'stiprumas_laipsn' => 180
     ],
     
     3 => [
-        'puse' => 'kaire',
-        'stiprumas_laipsn' => 50
+        'stiprumas_laipsn' => -50
     ],
     
     4 => [
-        'puse' => 'desine',
         'stiprumas_laipsn' => 180
     ],
     
     5 => [
-        'puse' => 'desine',
         'stiprumas_laipsn' => 45
     ],
 ];
